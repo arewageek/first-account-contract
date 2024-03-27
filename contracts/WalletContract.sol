16a170fa2a4945fb2c216bb26f1c386cf7608d2e
arewageek <arewageek@gmail.com>
2024-03-27 15:54:12 +0100

    Account contract mvp

--- contracts/WalletContract.sol
+++ contracts/WalletContract.sol
@@ -3,26 +3,54 @@
 pragma solidity ^0.8.19;
 
 contract Wallet {
-    address private _owner;
+    address public _owner;
     uint public balance;
+    address public _backupAccount;
 
-    constructor(){
+
+    constructor(address backupAccount){
         _owner = msg.sender;
+        _backupAccount = backupAccount;
     }
 
-    function deposit() public payable {
+
+    modifier OnlyOwner {
+        require (msg.sender == _owner);
+        _;
+    }
+
+
+    function deposit() public payable OnlyOwner {
         require(msg.value > 0, "Zero ethers tranaction not allowed");
         balance += msg.value;
     }
 
-    function withdraw(uint amount) public {
-        require(msg.sender == _owner, "Only owner can withdraw funds");
-        require(amount >= balance, "Insufficient Funds");
-        balance -= amount;
+    function withdraw() public OnlyOwner {
+        require(msg.sender == _owner, "Access denied");
+        (bool success,) = _owner.call{ value: address(this).balance }("");
+        require(success, "Failed");
     }
 
-    function transferOwnership(address _newOwner) public {
+    function transfer(uint amount) public OnlyOwner {
+        require(msg.sender == _owner, "Only owner can withdraw funds");
+        require(amount >= balance, "Insufficient Funds");
+        ( bool success, ) = _owner.call{ value: address(this).balance } ("");
+        require(success, "Failed");
+    }
+
+    function transferOwnership(address _newOwner) public OnlyOwner {
         require(msg.sender == _owner, "Access denied!");
         _owner = _newOwner;
+    }
+
+    function recoverAccount(address _newBackupAccount) public {
+        require(msg.sender == _backupAccount, "Access denied");
+        _owner = _backupAccount;
+        _backupAccount = _newBackupAccount;
+    }
+
+    function changeBackupAccount ( address _newBackupAccount) public OnlyOwner {
+        require (msg.sender == _owner);
+        _backupAccount = _newBackupAccount;
     }
 }
